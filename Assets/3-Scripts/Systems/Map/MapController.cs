beae212730a4e1ba8b5c899c1be352d9ef8dc67d
JGoodHub <jagoodmail@gmail.com>
2020-04-29 22:38:09 +0100

    [Map Generation] Created hierarchy/folder structure for game scene and map generation scripts

---
+++ Assets/3-Scripts/Systems/Map/MapController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour {
+
+    #region Inherited Methods
+
+    private void Start() {
+
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+
+    #region Coroutines
+
+    #endregion
+
+    #region Event Handlers
+
+    #endregion
+
+    #region Gizmos
+
+    //public bool drawGizmos;
+    private void OnDrawGizmos() {
+
+    }
+
+    #endregion
+
+}
