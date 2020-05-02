4693ea1018c2ca048c62b3131791557627fc5266
JGoodHub <jagoodmail@gmail.com>
2020-05-02 13:39:53 +0100

    [Map Gen] Added the ability to generate a hexagon map of tiles of a defined size

--- Assets/3-Scripts/Systems/Map/MapController.cs
+++ Assets/3-Scripts/Systems/Map/MapController.cs
@@ -4,37 +4,39 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour {
 
-    #region Inherited Methods
+    [System.Serializable]
+    public struct BiomeSet {
+        public string name;
+        public Sprite[] sprites;
+    }
+
+    public int seed;
+
+    public Transform tileContainer;
+    public GameObject[] tilePrefabs;
+
+    private HashSet<GameObject> tileObjects = new HashSet<GameObject>();
+
+    public Vector2Int boardSize;
 
     private void Start() {
+        Random.InitState(seed);
 
+        for (int y = 0; y < boardSize.y; y++) {
+            for (int x = 0; x < boardSize.x; x++) {
+                GameObject tileClone = Instantiate(tilePrefabs[0], GridToWorldPosition(new Vector2(x, y)), Quaternion.identity);
+                tileObjects.Add(tileClone);
+
+                tileClone.transform.parent = tileContainer;
+            }
+        }
     }
 
-    #endregion
-
-    #region Public Methods
-
-    #endregion
-
-    #region Private Methods
-
-    #endregion
-
-    #region Coroutines
-
-    #endregion
-
-    #region Event Handlers
-
-    #endregion
-
-    #region Gizmos
-
-    //public bool drawGizmos;
-    private void OnDrawGizmos() {
-
+    private Vector2 GridToWorldPosition(Vector2 position) {
+        return new Vector2((position.x * Tile.SIZE.x) + (position.y % 2 * (Tile.SIZE.x / 2)), position.y * (Tile.SIZE.y * 0.75f));
     }
 
-    #endregion
-
+    private Vector2 WorldToGridPosition(Vector2 position) {
+        return Vector2.zero;
+    }
 }
---
+++ Assets/3-Scripts/Systems/Map/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    /// <summary>
+    /// The constant size of all tiles in the game
+    /// </summary>
+    public static readonly Vector2 SIZE = new Vector2(1.2f, 1.4f);
+    
+}
