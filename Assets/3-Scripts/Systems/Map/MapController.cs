56bb2cfa818632b317381b74eb7e23db0dfc6020
JGoodHub <jagoodmail@gmail.com>
2020-05-02 22:43:34 +0100

    [Map Gen] Added a debug mode to visualize the perlin noise values

--- Assets/3-Scripts/Systems/Map/MapController.cs
+++ Assets/3-Scripts/Systems/Map/MapController.cs
@@ -18,25 +18,46 @@ public class MapController : MonoBehaviour {
     private HashSet<GameObject> tileObjects = new HashSet<GameObject>();
 
     public Vector2Int boardSize;
+    public float noiseScale = 5f;
 
     private void Start() {
         Random.InitState(seed);
 
         for (int y = 0; y < boardSize.y; y++) {
             for (int x = 0; x < boardSize.x; x++) {
-                GameObject tileClone = Instantiate(tilePrefabs[0], GridToWorldPosition(new Vector2(x, y)), Quaternion.identity);
+                GameObject tileClone = Instantiate(tilePrefabs[Random.Range(0, tilePrefabs.Length)], GridToWorldPosition(new Vector2Int(x, y)), Quaternion.identity);
                 tileObjects.Add(tileClone);
 
                 tileClone.transform.parent = tileContainer;
             }
         }
+
+        Camera.main.transform.position = (Vector3)GridToWorldPosition(new Vector2Int(boardSize.x / 2, boardSize.y / 2)) + (Vector3.back * 10);
     }
 
-    private Vector2 GridToWorldPosition(Vector2 position) {
+    private Vector2 GridToWorldPosition(Vector2Int position) {
         return new Vector2((position.x * Tile.SIZE.x) + (position.y % 2 * (Tile.SIZE.x / 2)), position.y * (Tile.SIZE.y * 0.75f));
     }
 
-    private Vector2 WorldToGridPosition(Vector2 position) {
-        return Vector2.zero;
+    private Vector2Int WorldToGridPosition(Vector2 position) {
+        return Vector2Int.zero;
+    }
+
+
+    public bool debug;
+    private void OnDrawGizmos() {
+        if (debug) {
+            Random.InitState(seed);
+
+            for (int y = 0; y < boardSize.y; y++) {
+                for (int x = 0; x < boardSize.x; x++) {
+                    Vector2 noiseSamplePosition = GridToWorldPosition(new Vector2Int(x, y));
+                    noiseSamplePosition *= noiseScale;
+                    float level = Mathf.Clamp01(Mathf.PerlinNoise(noiseSamplePosition.x, noiseSamplePosition.y));
+                    Gizmos.color = new Color(level, level, level);
+                    Gizmos.DrawSphere(GridToWorldPosition(new Vector2Int(x, y)), 0.5f);
+        }
+    }
+}
     }
 }
