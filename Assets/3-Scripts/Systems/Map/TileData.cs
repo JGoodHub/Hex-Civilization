aa637ba47f4247d5aa51bcdbed6a2666d1ebe987
JGoodHub <jagoodmail@gmail.com>
2020-11-15 20:05:35 +0000

    [Map Gen] Added base tiles and created basic prefabs for each

---
+++ Assets/3-Scripts/Systems/Map/TileData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : MonoBehaviour {
+    /// <summary> The constant size of all tiles in the game in unity units, used for positioning/laying out tiles. </summary>
+    public static readonly Vector2 SIZE = new Vector2(1.2f, 1.38f);
+
+    public enum Visibility {
+        HIDDEN,
+        DISCOVERED,
+        VISIBLE
+    }
+
+    public enum Type {
+        GRASS,
+        FLOWERS,
+        FOREST,
+        LOGGING,
+        FARM,
+        VILLAGE
+    }
+
+    public Type type;
+    private Visibility visibility;
+
+    [HideInInspector] public Vector2Int gridPosition;
+    [HideInInspector] public TileData[] adjacentTiles;
+
+    private void Start() {
+
+    }
+
+
+}
